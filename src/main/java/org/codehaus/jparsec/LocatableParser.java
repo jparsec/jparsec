d0b5a998fe1dd488f1a51dab4acbc25006af462b
Arnaud Bailly <abailly@murex.com>
2013-06-20 22:49:39 +0200

    basic implementation of Locatable logic #5
    * Locatable interface is called by successful parser when implemented by result

---
+++ src/main/java/org/codehaus/jparsec/LocatableParser.java
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * Copyright (C) Codehaus.org                                                *
+ * ------------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License");           *
+ * you may not use this file except in compliance with the License.          *
+ * You may obtain a copy of the License at                                   *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ *****************************************************************************/
+
+package org.codehaus.jparsec;
+
+class LocatableParser<T> extends Parser<T> {
+  private final Parser<T> parser;
+
+  public LocatableParser(Parser<T> parser) {
+    this.parser = parser;
+  }
+
+  @Override boolean apply(ParseContext ctxt) {
+    int begin = ctxt.getIndex();
+    if (!parser.apply(ctxt)) {
+      return false;
+    }
+
+    if(ctxt.result instanceof Locatable) {
+      Locatable locatable = (Locatable) ctxt.result;
+      locatable.setLocation(begin,ctxt.getIndex());
+      locatable.setSource(ctxt.source.subSequence(begin, ctxt.getIndex()).toString());
+    }
+
+    return true;
+  }
+
+  @Override public String toString() {
+    return "source";
+  }
+}
--- src/main/java/org/codehaus/jparsec/Parser.java
+++ src/main/java/org/codehaus/jparsec/Parser.java
@@ -475,6 +475,15 @@ public abstract class Parser<T> {
     return new ReturnSourceParser(this);
   }
 
+
+  /**
+   * A {@link Parser} that, when run successfully, annotates the result with the source of the parse and its location,
+   * provided the result is an instance of {@link Locatable}.
+   */
+  public final Parser<T> locate() {
+    return new LocatableParser(this);
+  }
+
   /**
    * A {@link Parser} that takes as input the {@link Token} collection returned by {@code lexer}, and runs {@code
    * this} to parse the tokens.
