2c7ff0376cd5cf8cccd7225292ef370d4b5ea61f
Arnaud Bailly <arnaud.oqube@gmail.com>
2013-08-29 22:07:09 +0200

    minor corrections to tests #4

--- src/test/java/org/codehaus/jparsec/PatternScannerTest.java
+++ src/test/java/org/codehaus/jparsec/PatternScannerTest.java
@@ -14,12 +14,13 @@ public class PatternScannerTest {
 
   @Test
   public void returns_done_when_parsing_incrementally_given_complete_input() throws Exception {
-    assertThat(Scanners.lineComment("#").incrementally()
-        .parse("# a comment").parse(" more input\n").isDone()).isTrue();
+    Parser.Incremental<Void> incremental = Scanners.lineComment("#").incrementally()
+        .parse("# a comment");
+    assertThat(incremental.parse(" more input\n").isDone()).isTrue();
   }
 
   @Test
-  public void returns_done_failed_parsing_incrementally_given_incorrect_input() throws Exception {
+  public void returns_failed_when_parsing_incrementally_given_incorrect_input() throws Exception {
     assertThat(Scanners.lineComment("#").incrementally().parse(" a comment\n").isFailed()).isTrue();
   }
 
