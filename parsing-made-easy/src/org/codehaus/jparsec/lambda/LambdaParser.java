40467068ee5d27bc7d16b3c153d6a8214ce45595
Arnaud Bailly <arnaud.oqube@gmail.com>
2013-09-10 18:48:44 +0200

    sprinkle optional whitespace all over application rule [pme]

--- parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
+++ parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
@@ -10,19 +10,20 @@ public class LambdaParser {
 
   // scanners
   private static final Parser<String> identifier = Scanners.IDENTIFIER;
-  
+
   private static final Parser<?> L_PAREN = token("(");
   private static final Parser<?> R_PAREN = token(")");
   private static final Parser<?> WS = _(Scanners.WHITESPACES);
-  
+  private static final Parser<?> WS_OPT = _(Scanners.WHITESPACES.optional());
+
   // parsers
   private static final Parser.Reference<Expr> lambdaRef = Parser.newReference();
-  
+
   private static final Parser<Expr> var = Mapper.curry(Var.class).sequence(identifier).cast();
-  
+
   private static final Parser<App> app = Mapper.curry(App.class)
-      .sequence(lambdaRef.lazy(), WS, lambdaRef.lazy())
-      .between(L_PAREN, R_PAREN);
+      .sequence(WS_OPT, lambdaRef.lazy(), WS_OPT, lambdaRef.lazy(), WS_OPT)
+      .between(L_PAREN, R_PAREN).between(WS_OPT, WS_OPT);
 
   private static final Parser<Expr> lambda = var.or(app);
 
@@ -33,9 +34,9 @@ public class LambdaParser {
   static {
     lambdaRef.set(lambda);
   }
-  
+
   public Expr parse(String input) {
     return lambda.parse(input);
   }
-  
+
 }
