d2feb9d6fd6fca171e7f0d356e940e80d96f3a1d
Arnaud Bailly <arnaud.oqube@gmail.com>
2013-09-10 17:28:49 +0200

    refactor to use Mapper [pme]

--- parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
+++ parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
@@ -2,18 +2,15 @@ package org.codehaus.jparsec.lambda;
 
 import org.codehaus.jparsec.Parser;
 import org.codehaus.jparsec.Scanners;
-import org.codehaus.jparsec.functors.Map;
+import org.codehaus.jparsec.misc.Mapper;
 import org.codehaus.jparsec.pattern.CharPredicates;
 
 public class LambdaParser {
+
+  private static final Parser<String> alpha = Scanners.isChar(CharPredicates.IS_ALPHA).source();
   
-  private static final Parser<Var> var = Scanners.isChar(CharPredicates.IS_ALPHA).source().map(new Map<String, Var>() {
-    @Override
-    public Var map(String s) {
-      return new Var(s);
-    }
-  });
-  
+  private static final Parser<Var> var = Mapper.curry(Var.class).sequence(alpha);
+
   public Var parse(String input) {
     return var.parse(input);
   }
