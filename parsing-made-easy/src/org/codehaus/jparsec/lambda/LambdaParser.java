6c5a01241b7c30f28aaa74e29cb7db4877645561
Arnaud Bailly <arnaud.oqube@gmail.com>
2013-09-10 17:45:55 +0200

    successfully parse application [pme]

--- parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
+++ parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
@@ -5,13 +5,20 @@ import org.codehaus.jparsec.Scanners;
 import org.codehaus.jparsec.misc.Mapper;
 import org.codehaus.jparsec.pattern.CharPredicates;
 
+import static org.codehaus.jparsec.misc.Mapper._;
+
 public class LambdaParser {
 
   private static final Parser<String> identifier = Scanners.isChar(CharPredicates.IS_ALPHA).many1().source();
   
   private static final Parser<Expr> var = Mapper.curry(Var.class).sequence(identifier).cast();
+
+  private static final Parser<?> L_PAREN = _(Scanners.isChar('('));
+  private static final Parser<?> R_PAREN = _(Scanners.isChar(')'));
+  private static final Parser<?> WS = _(Scanners.WHITESPACES);
   
-  private static final Parser<App> app = Mapper.curry(App.class).sequence(var,var);
+  private static final Parser<App> app = Mapper.curry(App.class)
+      .sequence(L_PAREN, var, WS, var, R_PAREN);
 
   private static final Parser<Expr> lambda = var.or(app);
   
