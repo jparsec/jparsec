a71a21174d121d6a3632e7c29a67c49a2d4896a9
Arnaud Bailly <arnaud.oqube@gmail.com>
2013-09-10 18:44:06 +0200

    minor refactoring, use standard IDENTIFIER parser [pme]

--- parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
+++ parsing-made-easy/src/org/codehaus/jparsec/lambda/LambdaParser.java
@@ -3,17 +3,16 @@ package org.codehaus.jparsec.lambda;
 import org.codehaus.jparsec.Parser;
 import org.codehaus.jparsec.Scanners;
 import org.codehaus.jparsec.misc.Mapper;
-import org.codehaus.jparsec.pattern.CharPredicates;
 
 import static org.codehaus.jparsec.misc.Mapper._;
 
 public class LambdaParser {
 
   // scanners
-  private static final Parser<String> identifier = Scanners.isChar(CharPredicates.IS_ALPHA).many1().source();
+  private static final Parser<String> identifier = Scanners.IDENTIFIER;
   
-  private static final Parser<?> L_PAREN = _(Scanners.isChar('('));
-  private static final Parser<?> R_PAREN = _(Scanners.isChar(')'));
+  private static final Parser<?> L_PAREN = token("(");
+  private static final Parser<?> R_PAREN = token(")");
   private static final Parser<?> WS = _(Scanners.WHITESPACES);
   
   // parsers
@@ -26,7 +25,11 @@ public class LambdaParser {
       .between(L_PAREN, R_PAREN);
 
   private static final Parser<Expr> lambda = var.or(app);
-  
+
+  private static Parser<?> token(String name) {
+    return _(Scanners.string(name));
+  }
+
   static {
     lambdaRef.set(lambda);
   }
