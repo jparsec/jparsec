13e0f14703dd75cf4d17afbcf2361eea1f1c7061
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-02-18 19:06:36 +0400

    Parser.withSource() parser
    Parser<T>.withSource() parser applies this parser and returns its
    result along with matches string.
    
    .token() parser could be used instead of .withSource(), however
    sometimes WithSource is more convenient, because global string
    object has to be stored somewhere to resolve token into string.
    
    Also, WithSource class is type-parameterized, unlike Token.

---
+++ src/main/java/org/codehaus/jparsec/WithSource.java
@@ -0,0 +1,45 @@
+package org.codehaus.jparsec;
+
+/**
+ * @author Stepan Koltsov
+ */
+public class WithSource<T> {
+  private final T value;
+  private final String source;
+
+  public WithSource(T value, String source) {
+    this.value = value;
+    this.source = source;
+  }
+
+  public T getValue() {
+    return value;
+  }
+
+  public String getSource() {
+    return source;
+  }
+
+  /** Returns the string representation of the token value. */
+  @Override public String toString() {
+    return String.valueOf(value);
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    WithSource that = (WithSource) o;
+
+    if (value != null ? !value.equals(that.value) : that.value != null) return false;
+    if (source != null ? !source.equals(that.source) : that.source != null) return false;
+
+    return true;
+  }
+
+  @Override public int hashCode() {
+    int result = value != null ? value.hashCode() : 0;
+    result = 31 * result + (source != null ? source.hashCode() : 0);
+    return result;
+  }
+}
