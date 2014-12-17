d448ec2281ceac97f6469044620ac194df7ec3e9
fluentfuture <benyu@benyu-macbookpro2.roam.corp.google.com>
2014-12-16 21:45:27 -0600

    Simplified SourceLocator

--- jparsec/src/main/java/org/codehaus/jparsec/SourceLocator.java
+++ jparsec/src/main/java/org/codehaus/jparsec/SourceLocator.java
@@ -31,10 +31,10 @@ import org.codehaus.jparsec.internal.util.IntList;
  */
 final class SourceLocator {
   
-  private final CharSequence source;
-  
   /** The line break character. */
-  private final char lineBreakChar;
+  private static final char LINE_BREAK = '\n';
+  
+  private final CharSequence source;
   
   /** The 0-based indices of the line break characters scanned so far. */
   @Private final IntList lineBreakIndices = new IntList(20);
@@ -52,19 +52,29 @@ final class SourceLocator {
   @Private int nextColumnIndex = 0;
   
   /**
-   * Uses binary search to look up the index of the first element in {@code ascendingInts} that's
-   * greater than or equal to {@code value}. If all elements are smaller than {@code value},
-   * {@code ascendingInts.size()} is returned.
+   * Creates a {@link SourceLocator} object.
+   * 
+   * @param source the source.
+   */  
+  SourceLocator(CharSequence source) {
+    this(source, 1, 1);
+  }
+  
+  /**
+   * Creates a {@link SourceLocator} object.
+   * 
+   * @param source the source.
+   * @param lineNumber the starting line number.
+   * @param columnNumber the starting column number.
    */
-  @Private static int binarySearch(IntList ascendingInts, int value) {
-    for (int begin = 0, to = ascendingInts.size();;) {
-      if (begin == to) return begin;
-      int i = (begin + to) / 2;
-      int x = ascendingInts.get(i);
-      if (x == value) return i;
-      else if (x > value) to = i;
-      else begin = i + 1;
-    }
+  @Private SourceLocator(CharSequence source, int lineNumber, int columnNumber) {
+    this.source = source;
+    this.startLineNumber = lineNumber;
+    this.startColumnNumber = columnNumber;
+  }
+  
+  Location locate(int index) {
+    return (index < nextIndex) ? lookup(index) : scanTo(index);
   }
   
   /**
@@ -97,7 +107,7 @@ final class SourceLocator {
     int columnIndex = nextColumnIndex;
     for (int i = nextIndex; i <= index; i++) {
       char c = source.charAt(i);
-      if (c == lineBreakChar) {
+      if (c == LINE_BREAK) {
         lineBreakIndices.add(i);
         columnIndex = 0;
       }
@@ -129,43 +139,19 @@ final class SourceLocator {
     return new Location(startLineNumber + l, (l == 0 ? startColumnNumber : 1) + c);
   }
   
-  Location locate(int index) {
-    return (index < nextIndex) ? lookup(index) : scanTo(index);
-  }
-  
   /**
-   * Creates a {@link SourceLocator} object.
-   * 
-   * @param source the source.
-   * @param lineNumber the starting line number.
-   * @param columnNumber the starting column number.
-   * @param lineBreakChar the line break character.
+   * Uses binary search to look up the index of the first element in {@code ascendingInts} that's
+   * greater than or equal to {@code value}. If all elements are smaller than {@code value},
+   * {@code ascendingInts.size()} is returned.
    */
-  public SourceLocator(
-      CharSequence source, int lineNumber, int columnNumber, char lineBreakChar) {
-    this.source = source;
-    this.lineBreakChar = lineBreakChar;
-    this.startLineNumber = lineNumber;
-    this.startColumnNumber = columnNumber;
-  }
-  
-  /**
-   * Creates a {@link SourceLocator} object.
-   * 
-   * @param source the source.
-   * @param lineNumber the starting line number.
-   * @param columnNumber the starting column number.
-   */
-  public SourceLocator(CharSequence source, int lineNumber, int columnNumber) {
-    this(source, lineNumber, columnNumber, '\n');
-  }
-  
-  /**
-   * Creates a {@link SourceLocator} object.
-   * 
-   * @param source the source.
-   */  
-  public SourceLocator(CharSequence source) {
-    this(source, 1, 1);
+  @Private static int binarySearch(IntList ascendingInts, int value) {
+    for (int begin = 0, to = ascendingInts.size();;) {
+      if (begin == to) return begin;
+      int i = (begin + to) / 2;
+      int x = ascendingInts.get(i);
+      if (x == value) return i;
+      else if (x > value) to = i;
+      else begin = i + 1;
+    }
   }
 }
