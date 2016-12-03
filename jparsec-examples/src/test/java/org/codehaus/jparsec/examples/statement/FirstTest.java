5b7f287a63b89a05356294ba438c7a4370eb9d76
ianrae <ianrae88@gmail.com>
2016-12-03 12:08:05 -0500

    reformat

--- jparsec-examples/src/test/java/org/codehaus/jparsec/examples/statement/FirstTest.java
+++ jparsec-examples/src/test/java/org/codehaus/jparsec/examples/statement/FirstTest.java
@@ -12,86 +12,88 @@ import org.junit.Test;
 
 public class FirstTest {
 
-	//low-level grammar
-	private static final Terminals OPERATORS =
-			Terminals.operators("=", "readonly", "var");
+    //low-level grammar
+    private static final Terminals OPERATORS = Terminals.operators("=", "readonly", "var");
 
-	//for integers and identifiers we need a PARSER and a TOKENIZER for each
-	//here are the parsers
-	public static Parser<String> identSyntacticParser = Terminals.Identifier.PARSER;
-	public static Parser<String> integerSyntacticParser = Terminals.IntegerLiteral.PARSER;
-	
-	//and here are the tokenizers, combined into our single tokenizer
-	static final Parser<?> TOKENIZER = Parsers.or(
-					OPERATORS.tokenizer(),
-					Terminals.IntegerLiteral.TOKENIZER, 
-					Terminals.Identifier.TOKENIZER, 
-					Terminals.Identifier.TOKENIZER);
+    //and here are the tokenizers, combined into our single tokenizer
+    private static final Parser<?> TOKENIZER = Parsers.or(
+            OPERATORS.tokenizer(),
+            Terminals.IntegerLiteral.TOKENIZER, 
+            Terminals.Identifier.TOKENIZER, 
+            Terminals.Identifier.TOKENIZER);
 
-	static final Parser<Void> IGNORED = Parsers.or(
-			Scanners.JAVA_LINE_COMMENT,
-			Scanners.JAVA_BLOCK_COMMENT,
-			Scanners.WHITESPACES).skipMany();	
-	
-	private static Parser<String> readonly() {
-		return OPERATORS.token("readonly").retn("readonly");
-	}
-	private static Parser<String> var() {
-		return OPERATORS.token("var").retn("var");
-	}
-	private static Parser<String> eq() {
-		return OPERATORS.token("=").retn("=");
-	}
+    private static final Parser<Void> IGNORED = Parsers.or(
+            Scanners.JAVA_LINE_COMMENT,
+            Scanners.JAVA_BLOCK_COMMENT,
+            Scanners.WHITESPACES).skipMany();	
 
-	//high-level grammar
-	
-	//parse single value into a SingleExpression
-	private static Parser<SingleExpression> singleExpression01() {
-		return Parsers.or(readonly())
-				.map(new org.codehaus.jparsec.functors.Map<String, SingleExpression>() {
-					@Override
-					public SingleExpression map(String arg0) {
-						return new SingleExpression(arg0);
-					}
-				});
-	}
-	
-	//parse two tokens but only take the output of last one in sequence()
-	private static Parser<SingleExpression> singleExpression02() {
-		return Parsers.sequence(readonly(), var()).map(SingleExpression::new);
-	}
-	
-	//parse two tokens and use the output of all tokens in sequence()
-	private static Parser<DoubleExpression> doubleExpression01() {
-		return Parsers.sequence(readonly(), var(), (String s, String s2) -> new DoubleExpression(s,s2));
-	}
-	//same thing but use Java 8 functional interface
-	private static Parser<DoubleExpression> doubleExpression02() {
-		return Parsers.sequence(readonly(), var(), DoubleExpression::new);
-	}
-	
-	@Test
-	public void testSingle1() {
-		SingleExpression exp = singleExpression01().from(TOKENIZER, IGNORED).parse("readonly");
-		assertEquals("readonly", exp.s);
-	}
-	@Test
-	public void testSingle2() {
-		SingleExpression exp = singleExpression02().from(TOKENIZER, IGNORED).parse("readonly var");
-		assertEquals("var", exp.s);
-	}
-	
-	@Test
-	public void testDouble1() {
-		DoubleExpression exp = doubleExpression01().from(TOKENIZER, IGNORED).parse("readonly var");
-		assertEquals("readonly", exp.s);
-		assertEquals("var", exp.s2);
-	}
-	@Test
-	public void testDouble2() {
-		DoubleExpression exp = doubleExpression02().from(TOKENIZER, IGNORED).parse("readonly var");
-		assertEquals("readonly", exp.s);
-		assertEquals("var", exp.s2);
-	}
+    private static Parser<String> readonly() {
+        return OPERATORS.token("readonly").retn("readonly");
+    }
+    private static Parser<String> var() {
+        return OPERATORS.token("var").retn("var");
+    }
+    private static Parser<String> eq() {
+        return OPERATORS.token("=").retn("=");
+    }
+
+    //high-level grammar
+
+    // *** Java 8 Syntax ***
+    //parse two tokens and use the output of all tokens in sequence()
+    private static Parser<DoubleExpression> doubleExpression01() {
+        return Parsers.sequence(readonly(), var(), (String s, String s2) -> new DoubleExpression(s,s2));
+    }
+    //same thing but use Java 8 functional interface
+    private static Parser<DoubleExpression> doubleExpression02() {
+        return Parsers.sequence(readonly(), var(), DoubleExpression::new);
+    }
+    
+    // *** Java 6 Syntax ***
+    //parse single value into a SingleExpression.
+    //This is an example of the older Java 6 syntax, using map.
+    //It is quite verbose (and is not recommended).
+    private static Parser<SingleExpression> singleExpression01() {
+        return Parsers.or(readonly())
+                .map(new org.codehaus.jparsec.functors.Map<String, SingleExpression>() {
+                    @Override
+                    public SingleExpression map(String arg0) {
+                        return new SingleExpression(arg0);
+                    }
+                });
+    }
+
+    // *** Mixed Syntax ***
+    //parse two tokens but only take the output of last one in sequence()
+    //Use map and Java 8 functional interface. It works but is not as simple
+    //as the Java 8 syntax shown above
+    private static Parser<SingleExpression> singleExpression02() {
+        return Parsers.sequence(readonly(), var()).map(SingleExpression::new);
+    }
+
+
+    @Test
+    public void testSingle1() {
+        SingleExpression exp = singleExpression01().from(TOKENIZER, IGNORED).parse("readonly");
+        assertEquals("readonly", exp.s);
+    }
+    @Test
+    public void testSingle2() {
+        SingleExpression exp = singleExpression02().from(TOKENIZER, IGNORED).parse("readonly var");
+        assertEquals("var", exp.s);
+    }
+
+    @Test
+    public void testDouble1() {
+        DoubleExpression exp = doubleExpression01().from(TOKENIZER, IGNORED).parse("readonly var");
+        assertEquals("readonly", exp.s);
+        assertEquals("var", exp.s2);
+    }
+    @Test
+    public void testDouble2() {
+        DoubleExpression exp = doubleExpression02().from(TOKENIZER, IGNORED).parse("readonly var");
+        assertEquals("readonly", exp.s);
+        assertEquals("var", exp.s2);
+    }
 
 }
