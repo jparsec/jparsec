707838784ed2444d4c5b8969d09e96e2249ba0c1
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-12-04 11:10:13 +0100

    replace Codehaus.org with jparsec.org everywhere

--- jparsec-examples/src/main/java/org/codehaus/jparsec/examples/java/ast/expression/BooleanLiteral.java
+++ jparsec-examples/src/main/java/org/codehaus/jparsec/examples/java/ast/expression/BooleanLiteral.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (C) Codehaus.org                                                *
+ * Copyright (C) jparsec.org                                                *
  * ------------------------------------------------------------------------- *
  * Licensed under the Apache License, Version 2.0 (the "License");           *
  * you may not use this file except in compliance with the License.          *
