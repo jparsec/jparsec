5f86ea740f37d1878d569247f3baa0d32a76e737
Ben Yu <yujige@gmail.com>
2016-12-04 22:13:26 -0600

    Revert "Revert "add statement example""

---
+++ jparsec-examples/src/main/java/org/codehaus/jparsec/examples/statement/ast/SingleExpression.java
@@ -0,0 +1,9 @@
+package org.codehaus.jparsec.examples.statement.ast;
+
+public class SingleExpression implements Expression {
+	public String s;
+	
+	public SingleExpression(String s) {
+		this.s = s;
+	}
+}
